b614da482651ee1ab784883b0342ce1939dabf22
kwb <wangbin@naver.com>
2020-02-03 20:18:12 +0900

    v0.2.1
    returnBorrowedBooks 생성

--- src/library/ellen/ellen.kt
+++ src/library/ellen/ellen.kt
@@ -1,89 +1,27 @@
 package library.ellen
 
 import library.ellen.data.*
+import kotlin.reflect.jvm.internal.impl.serialization.deserialization.ProtoContainer
 
 fun main() {
     makeBasicInformation()
-
-    searchBook("Holmes")
-    borrowBook("Ellen", "001")
+    borrowBook("001", "001")
     borrowBook("001", "002")
-    borrowBook("003", "003")
-    borrowBook("002", "002")
-    //returnBook("001")
-
-
-// 아빠답안지
-//    println("----------대출중------------------------")
-//    books.filter { it.borrowed }.forEach {
-//        println(it)
-//    }
-//
-//    println("----------대출실행------------------------")
-//    borrowBook("111", "002")
-//    borrowBook("001", "111")
-//
-//    borrowBook("001", "001")
-//    borrowBook("001", "002")
-//
-//
-//    println("----------사람마다빌린책------------------------")
-//    people.forEach {
-//        println("${it.name} -> ${it.borrowedBookImf}")
-//    }
-//
-//    println("-----------Holm검색 후 반납-----------------------")
-//    books.filter { it.name.contains("Holm") }.forEach {
-//        println(it)
-//        it.borrowed=false
-//        people.find { p -> p.borrowedBookImf.contains(it) }?.borrowedBookImf?.remove(it)
-//    }
-//
-//    println("----------대출실행------------------------")
-//    borrowBook("001", "003")
-//    borrowBook("003", "002")
-//
-//    println("----------사람마다빌린책------------------------")
-//    people.forEach {
-//        println("${it.name} -> ${it.borrowedBookImf}")
-//    }
-//
-//    println("-----------대출중인책리스트확인-----------------------")
-//    books.filter { it.borrowed }.forEach {
-//        println(it)
-//    }
-//
-//    println("----------사람마다빌린책------------------------")
-//    people.forEach {
-//        println("${it.name} -> ${it.borrowedBookImf}")
-//    }
-//
-//
-
+    returnBorrowedBooks("001")
 }
-
 
 fun searchBook(bookName: String): ArrayList<Book>? {
     var bookList = ArrayList<Book>()
     books.filter { it.name.contains(bookName) }.forEach { b ->
-        //        println(b.name)
+        //        L(b.name)
         bookList.add(b)
     }
     if (bookList != ArrayList<Book>()) {
-        println(bookList)
+        L(bookList)
         return bookList
     } else return null
 }
-// todo 완료됨
-
-
-//    for( i in 1..books.size){
-//        println(i)
-//    }
-//    books.forEach {
-//        println(it.name)
-//    }
-
+//완료됨
 
 fun borrowBook(personNumber: String, bookNumber: String) {
     var p = people.find { it.personNum == personNumber }
@@ -91,16 +29,43 @@ fun borrowBook(personNumber: String, bookNumber: String) {
 
     if (p != null) {
         if (b != null) {
-            if (b.borrowable == false) println("${b.name} 는 대출중인 책")
+            if (!b.borrowable) L("${b.name} 는 대출중인 책")
             else {
-                println("${p.name} 님 ${b.name} 가 대출됨")
+                L("${p.name} 님 ${b.name} 가 대출됨")
                 p.borrowedBookInfo.add(b)
                 b.borrowable = false
             }
-        } else println("책 번호가 존재하지 않음")
+        } else L("책 번호가 존재하지 않음")
 
-    } else println("회원 번호가 존재하지 않음")
+    } else L("회원 번호가 존재하지 않음")
 }
+// 완료됨
+
+fun returnBorrowedBooks(bookNumber: String) {
+    var b = books.find { it.bookNum == bookNumber }
+    var p = people.find { it.borrowedBookInfo.contains(b) }
+
+    if (b != null) {
+        if (p != null) {
+            b.borrowable = true
+            p.borrowedBookInfo.remove(b)
+            L("${p.name} 님 ${b.name} 가 반납되었습니다.")
+            println(p.borrowedBookInfo)
+        } else L("반납 불가능한 책")
+    } else L("책번호가 존재하지 않음")
+}
+
+
+fun L(str: String) {
+    println("ellen log : " + str)
+}
+
+fun L(arr: ArrayList<Book>) {
+    println("ellen log : " + arr)
+}
+
+//todo returnBorrowedBooks 확인 && checkBorrowedBook(bookNumber:String):Boolean 만들기
+
 
 
 // 아빠답안지
@@ -109,18 +74,18 @@ var p = people.find { it.personNum == personNumber }
 var b = books.find { b -> b.bookNum == bookNumber }
 
 if (b != null) {
-    if (b.borrowed) println("대출중")
+    if (b.borrowed) L("대출중")
     else {
         if (p != null) {
             p.borrowedBookImf.add(b)
             b.borrowed = true
-            println("${p.name} 님 ${b.name} 가 대출되었습니다")
+            L("${p.name} 님 ${b.name} 가 대출되었습니다")
         } else {
-            println("사람없음")
+            L("사람없음")
         }
     }
 } else {
-    println("책없음")
+    L("책없음")
 }
 */
 
@@ -128,25 +93,83 @@ if (b != null) {
 //  var borrowedcheck = false
 
 //    myloop@ for (i in 0 until books.size) {
-////        println(" i = $i")
+////        L(" i = $i")
 //        for (o in 0 until people.size) {
-////            println(" o = $o")
+////            L(" o = $o")
 //            if (personNumber == people[o].personNum && bookNumber == books[i].bookNum) {
 //                people[o].borrowedBook = books[i].name
 //                books[i].borrowed = false
 //                borrowedcheck = true
-//                println("${people[o].name}님 ${books[i].name}가 대출되었습니다.")
-//                println(people[o])
-//                println(books[i])
+//                L("${people[o].name}님 ${books[i].name}가 대출되었습니다.")
+//                L(people[o])
+//                L(books[i])
 //                break@myloop
 //            }
 //        }
 //    }
 //
 //    if (!borrowedcheck) {
-//        println("잘못된 입력값이 있습니다.")
+//        L("잘못된 입력값이 있습니다.")
 //    }
 //}
+
+fun runProgram() {}
+fun 대출하라책을() {}
+fun 반납하라책을() {}
+fun 검색해서출력하라책을() {}
+//예제
+
+// 아빠답안지
+//    }
+//        L("${it.name} -> ${it.borrowedBookImf}")
+//    people.forEach {
+//    L("----------사람마다빌린책------------------------")
+//
+//    }
+//        L(it)
+//    books.filter { it.borrowed }.forEach {
+//    L("-----------대출중인책리스트확인-----------------------")
+//
+//    }
+//        L("${it.name} -> ${it.borrowedBookImf}")
+//    people.forEach {
+//    L("----------사람마다빌린책------------------------")
+//
+//    borrowBook("003", "002")
+//    borrowBook("001", "003")
+//    L("----------대출실행------------------------")
+//
+//    }
+//        people.find { p -> p.borrowedBookImf.contains(it) }?.borrowedBookImf?.remove(it)
+//        it.borrowed=false
+//        L(it)
+//    books.filter { it.name.contains("Holm") }.forEach {
+//    L("-----------Holm검색 후 반납-----------------------")
+//
+//    }
+//        L("${it.name} -> ${it.borrowedBookImf}")
+//    people.forEach {
+//    L("----------사람마다빌린책------------------------")
+//
+//
+//    borrowBook("001", "002")
+//    borrowBook("001", "001")
+//
+//    borrowBook("001", "111")
+//    borrowBook("111", "002")
+//    L("----------대출실행------------------------")
+//
+//    }
+//        L(it)
+//    books.filter { it.borrowed }.forEach {
+//    L("----------대출중------------------------")
+//대출불가능일 때 -> false 리턴, 대출가능하면 true 리턴
+
+//        L(it.name)
+//    books.forEach {
+//    }
+//        L(i)
+//    for( i in 1..books.size){
 
 //fun returnBook(bookNumber: String) {
 //    var loopValue = 0
@@ -154,39 +177,12 @@ if (b != null) {
 //        if (bookNumber == people[i].borrowedBookImf) {
 //            people[i].borrowedBookImf = "None"
 //            books[i].borrowed = true
-//            println("${people[i]}님 반납이 완료되었습니다.")
+//            L("${people[i]}님 반납이 완료되었습니다.")
 //        }
 //    }
 //    if (books[loopValue].borrowed != false) {
-//        println("반납할 수 없는 책입니다.")
+//        L("반납할 수 없는 책입니다.")
 //    }
 //
 //}
 
-//fun returnBook(bookNumber: String){
-//    var b = books.find { b -> b.bookNum == bookNumber }
-//}
-
-
-fun runProgram() {}
-
-fun 대출하라책을() {}
-fun 반납하라책을() {}
-fun 검색해서출력하라책을() {}
-
-
-//대출불가능일 때 -> false 리턴, 대출가능하면 true 리턴
-//if (checkBook(inputValue) == false) {
-//    println("대출불가능")
-//}
-//else {
-//    println("대출가능")
-//}
-
-// todo returnBook 확인 & checkBorrowedBook(bookNumber:String):Boolean 만들기
-// todo 대출목록 쓰기
-
-
-fun returnBorrowedBooks(): ArrayList<Person> {
-    return ArrayList<Person>()
-}
